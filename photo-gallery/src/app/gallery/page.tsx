e5a96cce5cbd64c24e25fec48fe801bbd2f1b8e4
Aryan Pandey <aryan.lko04@gmail.com>
2024-04-03 01:00:36 +0530

    update the README.md with link to contributing guide

---
+++ photo-gallery/src/app/gallery/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+import { CldUploadButton } from "next-cloudinary";
+import {UploadResult} from "../page";
+import { Button } from "@/components/ui/button";
+
+export default function GalleryPage( ){
+    return(
+        <section>
+            <div className="flex justify-between">
+                <h1 className="text-4xl font-bold">Gallery</h1>
+                <Button asChild>
+                    <div className="flex gap-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+</svg>
+
+                <CldUploadButton onUpload={(result: UploadResult)=>{
+    
+}} uploadPreset="mzeqbgjf" />
+</div>
+                </Button>
+                
+            </div>
+        </section>
+    )
+}
