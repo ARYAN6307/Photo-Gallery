76f5cac6ebc3b4e59cf184190db63d4887eb7f97
Aryan Pandey <aryan.lko04@gmail.com>
2024-04-05 16:05:08 +0530

    committt

--- photo-gallery/src/app/gallery/page.tsx
+++ photo-gallery/src/app/gallery/page.tsx
@@ -1,25 +1,34 @@
-"use client";
-import { CldUploadButton } from "next-cloudinary";
-import {UploadResult} from "../page";
-import { Button } from "@/components/ui/button";
+import cloudinary from 'cloudinary'
+import UploadButton from "./uploadButton";
+import { CloudinaryImage } from './cloudinary-img';
 
-export default function GalleryPage( ){
+type SearchResult={
+    public_id:string;
+};
+
+
+export default async function GalleryPage( ){
+    const results=(await cloudinary.v2.search
+    .expression('resource_type:image')
+    .sort_by('public_id','desc')
+    .max_results(5)
+    .execute()) as { resources: SearchResult[]};
+
+
     return(
         <section>
             <div className="flex justify-between">
                 <h1 className="text-4xl font-bold">Gallery</h1>
-                <Button asChild>
-                    <div className="flex gap-2">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
-</svg>
+              <UploadButton/>
+                <div className="grid grid-cols-4 gap-4">
+                  {results.resources.map((result)=>(
+                    // eslint-disable-next-line react/jsx-key
+                    <CloudinaryImage
+                    key={result.public_id} src={result.public_id} width="400" height="300" alt="an image of something"
+                    />
+                  ))}
 
-                <CldUploadButton onUpload={(result: UploadResult)=>{
-    
-}} uploadPreset="mzeqbgjf" />
-</div>
-                </Button>
-                
+                </div>
             </div>
         </section>
     )
